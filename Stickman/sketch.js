b27eda7189a18f50ceba3f3a7584d7a6b04516c6
Sander J <home978.sj@gmail.com>
2020-10-08 09:27:24 +0200

    tilføjet stickman

---
+++ Stickman/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+
+
+function draw() {
+  let myColor = color (250, 230, 0)
+  fill(myColor);
+  ellipse(200, 100, 50);
+  line(200, 280, 200, 125);
+  line(160, 180, 200, 140);
+  line(200, 140, 240, 180);
+  line(200, 280, 230, 350);
+  line(200, 280, 170, 350);
+  let white = color (300, 300, 300)
+  fill(white);
+  ellipse(190, 95, 10, [10]);
+  ellipse(210, 95, 10, [10]);
+  noFill();
+  arc(200, 110, 30, 10, 0, 800);
+}
+
