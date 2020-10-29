eca1a974690ecc017c91491b9d9b0e5d6817090b
Sander J <home978.sj@gmail.com>
2020-10-29 08:47:36 +0100

    bounce ball

--- Bounce/sketch.js
+++ Bounce/sketch.js
@@ -1,11 +1,32 @@
+var x;
+var changeDirection;
+
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(500, 500);
+	x = 1;
+	changeDirection = false;
+	//denne variables rolle fungere som en "switch" som vælger hvilken
+	//retning cirklen skal gå efter.
 }
 
 function draw() {
   background(220);
-  x = mouseX
-  y = mouseY
+	ellipse(x,250,80);
   fill('red')
-  ellipse(x, y, 100)
+	if(x>width){
+		changeDirection=true}
+	//hvis cirklen går til den højre side, skifter den retning
+	//Effekten med retningen er nedenunder.
+	else if (x<=0){
+		changeDirection=false}
+	//hvis cirklen kommer forbi den venstre side (eller blive lige med 0)
+	//skifter den retning.
+	
+	if (x>=0 && changeDirection == false){
+		x=x+1}
+	//hvis x er større eller er lig med 0, skal den gå mod højre
+	else if(changeDirection == true){
+		x=x-1}
+	//Når "switch" har skiftet engang, så er x nødt til at være større end bredde
+	//så skal cirklen gå tilbage til venstre.
 }
